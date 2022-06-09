62748e17ca0e10f54d223950ffd57b6b1b03be4e
NguyenDucAnh2610 <97513336+NguyenDucAnh2610@users.noreply.github.com>
2022-06-09 07:46:44 +0700

    todoRedux

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "antd/dist/antd.min.css";
+import { Routes, Route } from "react-router-dom";
+import Homepage from "./pages/Homepage";
+import Detailpage from "./pages/DetailPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route exact path="/" element={<Homepage />}></Route>
+      <Route exact path="/detailpage/:id" element={<Detailpage />}></Route>
+    </Routes>
   );
 }
 
